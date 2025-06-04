b788e2499d54fc509f8f742a3e2aac5bb3fae64a
suching8848 <1579290250@qq.com>
2025-06-04 09:33:49 +0800

    6/4

---
+++ 1047.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuffer stack = new StringBuffer();
+        int top = -1;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (top >= 0 && stack.charAt(top) == ch) {
+                stack.deleteCharAt(top);
+                --top;
+            } else {
+                stack.append(ch);
+                ++top;
+            }
+        }
+        return stack.toString();
+    }
+}
---
+++ 933.java
@@ -0,0 +1,16 @@
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new ArrayDeque<Integer>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
+
